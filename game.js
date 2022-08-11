bd930792beda9965fe5ef41304eb834ba319920b
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2022-08-11 21:45:25 +0000

    add category template

--- game.js
+++ game.js
@@ -3,3 +3,24 @@ const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [];
+
+fetch("questions.json")
+    .then(res => {
+        return res.json();
+    })
+    .then(loadedQuestions => {
+        console.log(loadedQuestions);
+        questions = loadedQuestions;
+        startGame();
+    })
+    .catch( err => {
+        console.log(err);
+    });
