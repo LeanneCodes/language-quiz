b5ee2de0562c86f54bad87702bd51a26e2b091d6
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2022-08-24 19:41:35 +0000

    remove category from highscores html

--- highscores.js
+++ highscores.js
@@ -2,6 +2,6 @@ const highScoresList = document.getElementById('highScoresList');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 highScoresList.innerHTML = highScores.map(score => {
-    return `<li class="high-score">&nbsp&nbsp&nbsp${score.category} - ${score.name} - ${score.score}</li>`;
+    return `<li class="high-score">&nbsp&nbsp&nbsp${score.name} - ${score.score}</li>`;
 })
 .join("");
